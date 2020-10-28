79b3925c217c6cc6f17bb0b1d8868adc370fcf55
rakeshrio <49908435+rakeshrio@users.noreply.github.com>
2020-10-28 14:58:37 +0530

    'second-commit'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-
-      <router-view/>
-  
-
+    <navigation></navigation>
+    <router-view/>
+    <footer1></footer1>
   </div>
 </template>
+
+
 <script>
-// import header from './components/header'
+import navigation from './components/navigation'
+import footer1 from './components/footer1'
 export default {
+  name: 'app',
   data(){
     return{
-
+        demo:true
   }},
   components:{
-
-   
+   navigation,
+   footer1
   },
 }
 </script>
---
+++ src/components/navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-md-12">
+      <h1 class="text-center">header</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
